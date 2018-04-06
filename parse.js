a769408fa9df086914aaa126c4472930744506c6
anargahramanov <anargahramanov@gmail.com>
2018-04-06 18:42:50 +0400

    text and links from categories in files

--- parse.js
+++ parse.js
@@ -8,20 +8,39 @@ function scrapePage() {
 
         var url = 'https://www.w-t.az';
         var $ = cheerio.load(html);
-        var anar ="";
-         $(".nav a").each(function (element) {
-            var a = $(this).text();
-            var b = $(this).attr("href");
-            a += " - " + url + b;
-            anar += a;
+        var kategor = [];
+        var kategUrls = [];
+        $(".nav a").each(function (element) {
+            var kategText = $(this).text();
+            var kategUrl = $(this).attr("href");
+            kategUrls.push(url + kategUrl);
+            kategText += " - " + url + kategUrl;
+            kategor += kategText;
         });
 
-        console.log(anar);
-        
-        //write the entire scraped page to the local file system
+        kategUrls.forEach(function (a) {
+            var fileName = a.slice(22);
 
-        fs.writeFile('kateqoriyalar.txt', anar, function (err) {
-            console.log('entire-page.html successfully written to HTML folder');
+            //make an HTTP request for the page to be scraped
+            request(a, function (error, response, html) {
+                var $ = cheerio.load(html);
+                var url = 'https://www.w-t.az';
+                var items = [];
+                $(".item_a").each(function () {
+                    var mehsulText = $(this).text();
+                    var mehsulUrl = $(this).attr("href");
+                    var mehsulTextAndUrl = mehsulText + " - " + url + mehsulUrl; 
+                    items.push(mehsulTextAndUrl);
+                });
+
+                //write the entire scraped page to the local file system
+                fs.writeFile(fileName, items, function (err) {
+                    console.log('Writing new file successfully written to HTML folder');
+                });
+
+            });
+
+
         });
     });
 }
